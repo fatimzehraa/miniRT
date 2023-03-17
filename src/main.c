9f21e523ac418f8499e5891f7b80025c723217ac
Ismail Ait bella <iait-bel@e2r9p6.1337.ma>
2023-03-17 05:09:17 +0100

    fix: add parser to main

--- src/main.c
+++ src/main.c
@@ -4,27 +4,33 @@
 #include <mlx.h>
 #include "camera.h"
 #include <math.h>
+#include "parse.h"
 
 #include <stdio.h>
-int main()
+int main(int ac, char **av)
 {
 	t_ctx ctx;
-	t_shape *s2;
 
-	ctx.mlx = mlx_init();
-	ctx.win = mlx_new_window(ctx.mlx, WIN_SIDE, WIN_SIDE, "minirt");
+	if (ac != 2)
+		return EXIT_FAILURE;
+
 	ctx.s = NULL;
 
-	ctx.cam = camera((t_point){0,0,-1}, (t_vec){0,0,1}, M_PI/2);
-	ctx.lights = new_light((t_point){-3,0,5}, (t_vec){1,1,1});
-		t_vec v = (t_vec){0,1,0};
-	v = norm(v);
-	t_shape *s1= new_sphere((t_point){0, 0, 5}, 1.4);
-	s2 = new_cylinder((t_point){0, 0, 5}, v,1, 3);
-	add_back(&ctx.s, s1); 
-	//add_back(&ctx.s, s2); 
-	ctx.s->color = (t_vec){.5,0,0.3};
-	s2->color = (t_vec){0.80,1,.5};
+	ctx.cam = camera((t_point){0,0,-1}, (t_vec){0,0,1}, M_PI/4);
+	ctx.lights = new_light((t_point){0,5,3}, (t_vec){1,1,1});
+
+	ctx.s = parse(av[1]);
+	if (ctx.s == NULL)
+		return EXIT_FAILURE;
+
+	t_shape *s = ctx.s;
+	while (s != NULL)
+	{
+		print_shape(s);
+		s = s->next;
+	}
+	ctx.mlx = mlx_init();
+	ctx.win = mlx_new_window(ctx.mlx, WIN_SIDE, WIN_SIDE, "minirt");
 	render_(ctx);
 	mlx_loop(ctx.mlx);
 	
