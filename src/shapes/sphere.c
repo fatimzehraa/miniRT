12c8d6fb3f82b5f0fcb9f92de9e8a5e740ed9f21
fatimzehraa <58591844+fatimzehraa@users.noreply.github.com>
2023-03-24 03:06:25 +0000

    sphere shape && cmp w epsi instdf 0

--- src/shapes/sphere.c
+++ src/shapes/sphere.c
@@ -1,26 +1,26 @@
+#include "minirt.h"
 #include "shape.h"
 #include "stdio.h"
 t_equation sphere_intersection(t_ray r, t_shape *s)
 {
 	t_equation eq;
-	r.o.z = -2;
+	eq.shape = s;
 	t_vec c = sub(r.o, s->origin);
  	//printf("c.x = %f, c.y = %f, c.z = %f\n", s->origin.x, s.origin.y, s.origin.z);
 	eq.a = vec_dot(r.dir, r.dir);
 	eq.b = 2 * vec_dot(r.dir, c);
 	eq.c = vec_dot(c, c) - pow(s->r, 2);
 	eq.delta = powf(eq.b, 2) - 4 * eq.a * eq.c;
-	if (eq.delta < 0)
+	if (eq.delta < -EPSILON)
 		return (eq);
 	eq.t1 = (-eq.b + sqrt(eq.delta)) / (2 * eq.a);
 	eq.t2 = (-eq.b - sqrt(eq.delta)) / (2 * eq.a);
-	if (eq.t1 < 0 && eq.t2 < 0)
+	if (eq.t1 < EPSILON && eq.t2 < EPSILON)
 		return (eq);
-	if (eq.t1 < eq.t2)
+	if (eq.t1 > EPSILON && eq.t1 < eq.t2)
 		eq.t = eq.t1;
 	else
 		eq.t = eq.t2;
-	eq.shape = s;
 	return (eq);
 }
 
